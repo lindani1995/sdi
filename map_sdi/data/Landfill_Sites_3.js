030659da0da8c90775fa3d43c7c997853f9bb613
lindani1995 <85552429+lindani1995@users.noreply.github.com>
2021-06-09 00:28:02 +0200

    Add files via upload

---
+++ map_sdi/data/Landfill_Sites_3.js
@@ -0,0 +1,25 @@
+var json_Landfill_Sites_3 = {
+"type": "FeatureCollection",
+"name": "Landfill_Sites_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": 1.0, "NAME": "Kraaifontein", "STATUS": "Closed", "ADRS": "Okavango Rd", "TYPE": "Landfill" }, "geometry": { "type": "Point", "coordinates": [ 18.706407938911251, -33.821501284953953 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 2.0, "NAME": "Table View", "STATUS": "Closed", "ADRS": "Tryall Rd", "TYPE": "Landfill" }, "geometry": { "type": "Point", "coordinates": [ 18.502488601157776, -33.790753901705372 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 3.0, "NAME": "Brackenfell", "STATUS": "Closed", "ADRS": "Reservoir Rd", "TYPE": "Landfill" }, "geometry": { "type": "Point", "coordinates": [ 18.712947803724543, -33.878459079195352 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 4.0, "NAME": "Gordons Bay", "STATUS": "Closed", "ADRS": "Main Rd", "TYPE": "Landfill" }, "geometry": { "type": "Point", "coordinates": [ 18.84739243786942, -34.1385747044799 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 5.0, "NAME": "Faure", "STATUS": "Closed", "ADRS": "Old Main Rd (r102)", "TYPE": "Landfill" }, "geometry": { "type": "Point", "coordinates": [ 18.696360277407315, -34.014181391432622 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 6.0, "NAME": "Atlantis", "STATUS": "Proposed", "ADRS": "Donkergat Farm", "TYPE": "Landfill" }, "geometry": { "type": "Point", "coordinates": [ 18.483028918988939, -33.647624520672409 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 7.0, "NAME": "Kalbaskraal", "STATUS": "Proposed", "ADRS": "Bottelfontein Farm", "TYPE": "Landfill" }, "geometry": { "type": "Point", "coordinates": [ 18.653495951785882, -33.636203695331133 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 8.0, "NAME": "Coastal Park", "STATUS": "Current", "ADRS": "Baden Powell Drive", "TYPE": "Landfill" }, "geometry": { "type": "Point", "coordinates": [ 18.502480535084018, -34.091139464410013 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 9.0, "NAME": "Bellville", "STATUS": "Closed", "ADRS": "Sacks Circle", "TYPE": "Landfill" }, "geometry": { "type": "Point", "coordinates": [ 18.653268331544677, -33.938859961192371 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 10.0, "NAME": "Vissershok  South", "STATUS": "Current", "ADRS": "Franksdale Road", "TYPE": "Landfill" }, "geometry": { "type": "Point", "coordinates": [ 18.542324554157531, -33.777686990965123 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 11.0, "NAME": "Kraaifontein", "STATUS": "Current", "ADRS": "N1 Freeway", "TYPE": "Waste Management Facility" }, "geometry": { "type": "Point", "coordinates": [ 18.739085479804636, -33.838732518086168 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 12.0, "NAME": "Swartklip", "STATUS": "Closed", "ADRS": "Swartklip Rd", "TYPE": "Landfill" }, "geometry": { "type": "Point", "coordinates": [ 18.648237315460879, -34.052068257499812 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 13.0, "NAME": "Swartklip", "STATUS": "Current", "ADRS": "Swartklip Road", "TYPE": "Refuse Transfer Station" }, "geometry": { "type": "Point", "coordinates": [ 18.64915111259609, -34.049272222260086 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 14.0, "NAME": "Athlone", "STATUS": "Current", "ADRS": "Settlers Way", "TYPE": "Refuse Transfer Station" }, "geometry": { "type": "Point", "coordinates": [ 18.515710877456339, -33.948521890041391 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 15.0, "NAME": "Vissershok North", "STATUS": "Current", "ADRS": "Franksdale Road", "TYPE": "Landfill" }, "geometry": { "type": "Point", "coordinates": [ 18.54319287581232, -33.770946853711429 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 16.0, "NAME": "Coastal Park", "STATUS": "Planned", "ADRS": "Baden Powell Drive", "TYPE": "Refuse Transfer Station" }, "geometry": { "type": "Point", "coordinates": [ 18.499567417925547, -34.093107301886398 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 17.0, "NAME": "Helderberg", "STATUS": "Planned", "ADRS": "Main", "TYPE": "Refuse Transfer Station" }, "geometry": { "type": "Point", "coordinates": [ 18.798918958664927, -34.058794831138222 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 18.0, "NAME": "Bellville", "STATUS": "Current", "ADRS": "Sacks Circle", "TYPE": "Waste Management Facility" }, "geometry": { "type": "Point", "coordinates": [ 18.649011796294243, -33.932000454140301 ] } }
+]
+}
